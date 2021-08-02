703adc0402a29ecbe0825e6225f98b1e2e27b172
NVombat <nvombatkere@gmail.com>
2021-08-02 10:06:47 +0530

    ReadMe Update

--- face_detector.py
+++ face_detector.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 #Using grayscale increases accuracy of finding faces so Convert the image to grayscale
-img = cv2.imread("Practice/images/face.jpg")
+img = cv2.imread("Practice/images/test1.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #Use the classifier to detect faces
@@ -16,7 +16,7 @@ for x,y,w,h in faces:
     #Draw a rectangle around the faces and give it a color and width
     img = cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
 
-# # Resize the image to quarter the original size using shape attribute
+# # Resize the image to quarter the original size using shape attributeq
 # resized_img = cv2.resize(img,(int(img.shape[1]/4), int(img.shape[0]/4)))
 
 cv2.imshow("Detected Face", img)
--- full_body_detector.py
+++ full_body_detector.py
@@ -4,19 +4,19 @@ import cv2
 #Creates a cascade classifier object which allows us to search for a full body in an image
 full_body_cascade = cv2.CascadeClassifier('/home/nvombat/Desktop/WebCam-Motion-Detector/haarcascade_fullbody_default.xml')
 
-img = cv2.imread("Practice/images/people1.jpg")
+img = cv2.imread("Practice/images/people2.jpg")
 #print(type(img))
 #print(img)
 
 # Resize the image to quarter the original size
-resized_img = cv2.resize(img,(int(img.shape[1]/4), int(img.shape[0]/4)))
+resized_img = cv2.resize(img,(int(img.shape[1])/4, int(img.shape[0]/4)))
 
 #Convert the image to grayscale
 gray_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY) 
 #print(gray_img)
 
 #Use the classifier to detect bodies
-bodies = full_body_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=3)
+bodies = full_body_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=0)
 #print(type(bodies))
 #print(bodies)
 
